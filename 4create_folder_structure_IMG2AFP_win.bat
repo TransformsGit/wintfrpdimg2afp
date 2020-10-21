2513aa310c159c11d89799c2e6839d5466f3e5a1
TransformsGit <64213593+TransformsGit@users.noreply.github.com>
2020-10-21 12:24:46 +0300

    Add files via upload

--- 4create_folder_structure_IMG2AFP_win.bat
+++ 4create_folder_structure_IMG2AFP_win.bat
@@ -8,12 +8,16 @@ Echo Deleting all folders from %dir%
 for /d %%p in (%dir%\*) Do rd /Q /S "%%p"
 @echo Folder deleted.
 
+mkdir E:\OutputTransformIMG2AFP\win\output\out_AFP\TestFiles
+echo mkdir E:\OutputTransformIMG2AFP2win\output\out_AFP\TestFiles
+
 mkdir E:\OutputTransformIMG2AFP\win\output\out_PDF\TestFiles
 echo mkdir E:\OutputTransformIMG2AFP2win\output\out_PDF\TestFiles
 
 mkdir E:\OutputTransformIMG2AFP\win\output\out_TIFF\MasterFiles
 echo mkdir E:\OutputTransformIMG2AFP\win\output\out_TIFF\MasterFiles
 
+c:\Windows\System32\xcopy.exe /T /e e:\TestFiles_folder_structure\TestFiles\* E:\OutputTransformIMG2AFP\win\output\out_AFP\TestFiles\ /s /i
 c:\Windows\System32\xcopy.exe /T /e e:\TestFiles_folder_structure\TestFiles\* E:\OutputTransformIMG2AFP\win\output\out_PDF\TestFiles\ /s /i
 c:\Windows\System32\xcopy.exe /T /e e:\TestFiles_folder_structure\TestFiles\* E:\OutputTransformIMG2AFP\win\output\out_TIFF\MasterFiles\ /s /i
 
